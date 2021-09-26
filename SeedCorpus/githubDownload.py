dd3adea733ff5685d75652e1def4d03236af1a32
Jin <54jin.huang@gmail.com>
2021-09-26 15:03:56 +0800

    Add magic compare

--- SeedCorpus/githubDownload.py
+++ SeedCorpus/githubDownload.py
@@ -1,15 +1,4 @@
 #coding=utf-8
-# This script allows to crawl information and repositories from GitHub using the GitHub REST API (https://developer.github.com/v3/search/).
-#
-# Given a query, the script downloads for each repository returned by the query its ZIP file.
-# In addition, it also generates a CSV file containing the list of repositories queried.
-# For each query, GitHub returns a json file which is processed by this script to get information about repositories.
-#
-# The GitHub API limits the queries to get 100 elements per page and up to 1,000 elements in total.
-# To get more than 1,000 elements, the main query should be splitted in multiple subqueries using different time windows through the constant SUBQUERIES (it is a list of subqueries).
-#
-# As example, constant values are set to get the repositories on GitHub of the user 'rsain'.
-
 
 #############
 # Libraries #
@@ -25,7 +14,7 @@ import sys
 import pickle
 import traceback
 import concurrent.futures
-
+import codecs
 #############
 # Constants #
 #############
@@ -44,24 +33,42 @@ g_proxy = {
 # Functions #
 #############
 
-
-def download_file(sha,url,extension,store_dir):
+# ToDo:
+# don't download file bigger then MAX_SIZE
+def download_file(sha,url,extension,store_dir,magic):
     global g_total_download, g_proxy
     
     file_name = "%s.%s" %(sha,extension)
-    
+    magic_flag = 0
     if os.path.exists(os.path.join(store_dir, file_name)):
-        return (1, ("%s has exitsted\n"%sha))
+        return (1, ("%s exitsted\n"%sha))
     
     try:
-        with requests.get(url, stream=True, proxies = g_proxy) as r:
-            with open(os.path.join(store_dir, file_name), 'wb') as f:
+        #with requests.get(url, stream=True, proxies = g_proxy) as r:
+        with requests.get(url, stream=True) as r:
+            if magic != None:
+                magic_flag = 1
+            store_name = os.path.join(store_dir, file_name)
+            with open(store_name, 'wb') as f:
                 for data in r.iter_content(1024):
+                    if magic_flag and not data.startswith( codecs.decode(magic,'hex_codec')):
+                        f.close()
+                        os.remove(store_name)
+                        return (0,"Magic is not right:\nDownload file magic is %s\n%s\n" % (data[:10],url))
+                    else:
+                        magic_flag = 0
+                        
                     f.write(data)
-                    return (1, "%s has been downloaded\n" % sha )
+                f.close()
+                return (1, "%s has been downloaded\n" % sha )
+                
     except KeyboardInterrupt:
+        f.close()
+        os.remove(store_name)
         return (0, "%s download failed\n %s" % (url,traceback.print_exc()) )
     except :
+        f.close()
+        os.remove(store_name)
         return (0, "%s download failed\n %s" % (url,traceback.print_exc()) )
         
 
@@ -87,18 +94,21 @@ def usage():
    
 if __name__ == "__main__":
     
-    
-    if len(sys.argv) != 3:
+    magic = None
+    if len(sys.argv) < 3:
         usage()
         sys.exit(0)
     else:
         ext = sys.argv[1]
         store_folder = sys.argv[2]
+        if len(sys.argv) > 3:
+            magic = sys.argv[3]
+
         initialize_url_list(ext)
     
-    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
         try:
-            futures = [executor.submit(download_file, k,v,ext,store_folder) for (k,v) in g_download_url_list.items()]
+            futures = [executor.submit(download_file, k,v,ext,store_folder,magic) for (k,v) in g_download_url_list.items()]
             for future in concurrent.futures.as_completed(futures):
                 if future.result()[0]:
                     g_total_download = g_total_download+1
